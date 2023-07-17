a562fc5fe75e8109d3c4e8c8490b5e697adc8e82
Esmaeel <e.cheshmekhavar@hostiran.com>
2023-07-17 16:18:07 +0330

    HW-6138 Add exception files

---
+++ lang/fa/exceptions.php
@@ -0,0 +1,10 @@
+<?php
+
+
+use App\Exceptions\Base\ExceptionCodes;
+
+return [
+    ExceptionCodes::REPOSITORY_DELETE_MODEL => 'مدل :model :id را نمی توان حذف نمود.',
+    ExceptionCodes::REPOSITORY_MODEL_NOT_FOUND => 'مدل مورد نظر یافت نشد.',
+    ExceptionCodes::LOCKED_INVOICE_ALREADY_IMPORTED_TO_RAHKARAN => 'فاکتور :invoice_id قابل ویرایش نیست.',
+];
