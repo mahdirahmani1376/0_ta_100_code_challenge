37857762693c7bb5ee36247e2d68b6cc7a2a4d38
Esmaeel <e.cheshmekhavar@hostiran.com>
2023-07-17 16:21:13 +0330

    HW-6138 Implement sumOfPaidTransactions function in TransactionRepository

--- app/Repositories/Transaction/TransactionRepository.php
+++ app/Repositories/Transaction/TransactionRepository.php
@@ -2,11 +2,24 @@
 
 namespace App\Repositories\Transaction;
 
+use App\Models\Invoice;
 use App\Models\Transaction;
 use App\Repositories\Base\BaseRepository;
 use App\Repositories\Transaction\Interface\TransactionRepositoryInterface;
+use Illuminate\Contracts\Container\BindingResolutionException;
 
 class TransactionRepository extends BaseRepository implements TransactionRepositoryInterface
 {
     public string $model = Transaction::class;
+
+    /**
+     * @throws BindingResolutionException
+     */
+    public function sumOfPaidTransactions(Invoice $invoice): int
+    {
+        return $this->newQuery()
+            ->where('invoice_id', $invoice->getKey())
+            ->where('status', Transaction::STATUS_SUCCESS)
+            ->sum('amount');
+    }
 }
