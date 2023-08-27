2f7e477caedeec56835e5690e187712e8344d304
Esmaeel <e.cheshmekhavar@hostiran.com>
2023-08-27 13:25:20 +0330

    IT-6509 Use ProcessInvoiceAction inside StoreInvoiceAction when Invoice is made as REFUND

--- app/Actions/Admin/Invoice/StoreInvoiceAction.php
+++ app/Actions/Admin/Invoice/StoreInvoiceAction.php
@@ -2,6 +2,7 @@
 
 namespace App\Actions\Admin\Invoice;
 
+use App\Models\Invoice;
 use App\Services\Admin\Invoice\Item\StoreItemService;
 use App\Services\Admin\Invoice\StoreInvoiceService;
 use App\Services\Invoice\CalcInvoicePriceFieldsService;
@@ -11,15 +12,18 @@ class StoreInvoiceAction
     private StoreInvoiceService $storeInvoiceService;
     private StoreItemService $storeItemService;
     private CalcInvoicePriceFieldsService $calcInvoicePriceFieldsService;
+    private ProcessInvoiceAction $processInvoiceAction;
 
     public function __construct(StoreInvoiceService           $storeInvoiceService,
                                 StoreItemService              $storeItemService,
                                 CalcInvoicePriceFieldsService $calcInvoicePriceFieldsService,
+                                ProcessInvoiceAction          $processInvoiceAction,
     )
     {
         $this->storeInvoiceService = $storeInvoiceService;
         $this->storeItemService = $storeItemService;
         $this->calcInvoicePriceFieldsService = $calcInvoicePriceFieldsService;
+        $this->processInvoiceAction = $processInvoiceAction;
     }
 
     public function __invoke(array $data)
@@ -31,10 +35,12 @@ class StoreInvoiceAction
                 ($this->storeItemService)($invoice, $item);
             }
         }
-        // Calculate sub_total, tax, total fields of invoice
+
         $invoice = ($this->calcInvoicePriceFieldsService)($invoice);
-        // TODO check if invoice is paid refunded or ...
-//        ($this->processInvoiceAction)($invoice);
+
+        if ($invoice->status == Invoice::STATUS_REFUNDED) {
+            ($this->processInvoiceAction)($invoice);
+        }
 
         return $invoice;
     }
