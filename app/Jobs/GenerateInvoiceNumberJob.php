458093b531f176c7a645ff5128d88229d08d1a96
Esmaeel <e.cheshmekhavar@hostiran.com>
2023-08-27 13:19:45 +0330

    IT-6509 Implement GenerateInvoiceNumberJob

---
+++ app/Jobs/GenerateInvoiceNumberJob.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Repositories\Invoice\Interface\InvoiceNumberRepositoryInterface;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Cache;
+
+class GenerateInvoiceNumberJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public string $type;
+    public string $fiscalYear;
+
+    public function __construct(string $type, string $fiscalYear)
+    {
+        $this->type = $type;
+        $this->fiscalYear = $fiscalYear;
+    }
+
+    public function handle(InvoiceNumberRepositoryInterface $invoiceNumberRepository): void
+    {
+        $unusedCount = $invoiceNumberRepository->countUnused($this->type, $this->fiscalYear);
+        $threshold = 500; // TODO read this from config or something
+        if ($unusedCount < $threshold) {
+            $lock = Cache::lock('generateInvoiceNumberLock', 10);
+            if ($lock->get()) {
+                Artisan::call('app:generate-invoice-number', [
+                    '--type' => $this->type,
+                    '--fiscal-year' => $this->fiscalYear,
+                    '--count' => $threshold,
+                    '--lock-owner' => $lock->owner(),
+                ]);
+            }
+        }
+    }
+}
