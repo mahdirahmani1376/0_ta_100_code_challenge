da50ed9b7701666076ba9c323e60c35c7ae19187
Esmaeel <e.cheshmekhavar@hostiran.com>
2023-08-27 13:07:23 +0330

    IT-6509 Create AssignInvoiceNumberCommand.php

---
+++ app/Console/Commands/AssignInvoiceNumberCommand.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Services\Admin\Invoice\AssignInvoiceNumberService;
+use App\Services\Invoice\FindInvoiceByIdService;
+use Illuminate\Console\Command;
+
+class AssignInvoiceNumberCommand extends Command
+{
+    protected $signature = 'app:assign-invoice-number {invoice*}';
+
+    protected $description = 'Assign InvoiceNumber to one or more Invoice(s)';
+
+    public function handle(AssignInvoiceNumberService $assignInvoiceNumberService, FindInvoiceByIdService $findInvoiceByIdService)
+    {
+        $invoiceIds = $this->argument('invoice');
+        $this->alert('Assign InvoiceNumber to Invoice, count: ' . count($invoiceIds));
+
+        $this->withProgressBar($invoiceIds, function ($invoiceId) use ($assignInvoiceNumberService, $findInvoiceByIdService) {
+            $invoice = $findInvoiceByIdService($invoiceId);
+            $assignInvoiceNumberService($invoice);
+        });
+
+        $this->newLine();
+    }
+}
