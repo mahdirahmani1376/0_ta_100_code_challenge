89d7653cd36d25c1a52f69635a1fc429590b1103
Esmaeel <e.cheshmekhavar@hostiran.com>
2023-08-29 19:07:34 +0330

    IT-6623 Implement profile ApplyBalanceToInvoiceAction

---
+++ app/Actions/Profile/Invoice/ApplyBalanceToInvoiceAction.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Actions\Profile\Invoice;
+
+use App\Actions\Admin\Invoice\ApplyBalanceToInvoiceAction as AdminApplyBalanceToInvoiceActionAlias;
+
+class ApplyBalanceToInvoiceAction extends AdminApplyBalanceToInvoiceActionAlias
+{
+}
