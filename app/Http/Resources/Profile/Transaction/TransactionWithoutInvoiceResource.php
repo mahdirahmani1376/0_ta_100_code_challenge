64bda8eb49b35cecddcd444263b7f13d85ad9b60
Esmaeel <e.cheshmekhavar@hostiran.com>
2023-08-29 19:13:24 +0330

    IT-6623 Implement profile TransactionWithoutInvoiceResource.php

---
+++ app/Http/Resources/Profile/Transaction/TransactionWithoutInvoiceResource.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Http\Resources\Profile\Transaction;
+
+use App\Http\Resources\Admin\Transaction\TransactionWithoutInvoiceResource as AdminTransactionWithoutInvoiceResourceAlias;
+
+class TransactionWithoutInvoiceResource extends AdminTransactionWithoutInvoiceResourceAlias
+{
+}
