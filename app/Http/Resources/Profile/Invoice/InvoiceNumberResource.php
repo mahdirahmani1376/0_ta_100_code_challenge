02ad22c24c401c5c5db4fd582d25a424c8fee62a
Esmaeel <e.cheshmekhavar@hostiran.com>
2023-08-29 19:12:25 +0330

    IT-6623 Implement profile DownloadInvoiceOfficialBill

---
+++ app/Http/Resources/Profile/Invoice/InvoiceNumberResource.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Http\Resources\Profile\Invoice;
+
+use App\Http\Resources\Admin\Invoice\InvoiceNumberResource as AdminInvoiceNumberResourceAlias;
+
+class InvoiceNumberResource extends AdminInvoiceNumberResourceAlias
+{
+}
