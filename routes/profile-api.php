85d354f55acd81eb32dfd4d210e85fc559ca4aca
Esmaeel <e.cheshmekhavar@hostiran.com>
2023-08-29 19:15:30 +0330

    IT-6623 Implement missing profile routes - /everything - /apply-balance - /download (download bill) - /cancel

--- routes/profile-api.php
+++ routes/profile-api.php
@@ -6,24 +6,33 @@ use App\Http\Controllers\Profile\ClientBankAccount\UpdateClientBankAccountContro
 use App\Http\Controllers\Profile\ClientCashout\IndexClientCashoutController;
 use App\Http\Controllers\Profile\ClientCashout\StoreClientCashoutController;
 use App\Http\Controllers\Profile\ClientCashout\UpdateClientCashoutController;
+use App\Http\Controllers\Profile\Invoice\ApplyBalanceToInvoiceController;
+use App\Http\Controllers\Profile\Invoice\CancelInvoiceController;
 use App\Http\Controllers\Profile\Invoice\DeleteOfflineTransactionController;
+use App\Http\Controllers\Profile\Invoice\DownloadInvoiceBillController;
 use App\Http\Controllers\Profile\Invoice\IndexInvoiceController;
 use App\Http\Controllers\Profile\Invoice\ShowInvoiceController;
 use App\Http\Controllers\Profile\Invoice\StoreMassPaymentInvoiceController;
 use App\Http\Controllers\Profile\Invoice\StoreOfflineTransactionController;
+use App\Http\Controllers\Profile\ListEverythingController;
 use App\Http\Controllers\Profile\Transaction\IndexTransactionController;
 use App\Http\Controllers\Profile\Wallet\AddBalanceController;
 use App\Http\Controllers\Profile\Wallet\ShowWalletController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('everything', ListEverythingController::class);
+
 Route::namespace('Invoice')
     ->prefix('invoice')
     ->group(function () {
         Route::get('/', IndexInvoiceController::class);
-        Route::get('{profileInvoice}', ShowInvoiceController::class);
-        Route::post('{profileInvoice}/offline-transaction', StoreOfflineTransactionController::class);
-        Route::delete('{profileInvoice}/offline-transaction/{profileOfflineTransaction}', DeleteOfflineTransactionController::class);
         Route::post('mass-payment', StoreMassPaymentInvoiceController::class);
+        Route::get('{profileInvoice}', ShowInvoiceController::class);
+        Route::post('{profileInvoice}/apply-balance', ApplyBalanceToInvoiceController::class);
+        Route::get('{profileInvoice}/download', DownloadInvoiceBillController::class);
+        Route::post('{profileInvoice}/cancel', CancelInvoiceController::class);
+        Route::post('{profileInvoice}/offline-transaction', StoreOfflineTransactionController::class);
+        Route::delete('offline-transaction/{profileOfflineTransaction}', DeleteOfflineTransactionController::class);
     });
 
 Route::namespace('Wallet')
