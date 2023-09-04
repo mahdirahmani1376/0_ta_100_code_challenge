9d70f3dd99502fdc84d901581464119f9aeb9dee
Esmaeel <e.cheshmekhavar@hostiran.com>
2023-09-04 18:18:14 +0330

    IT-6727 Implement missed business logic regarding changing Invoice status

--- app/Http/Requests/Admin/Invoice/ChangeInvoiceStatusRequest.php
+++ app/Http/Requests/Admin/Invoice/ChangeInvoiceStatusRequest.php
@@ -16,17 +16,39 @@ class ChangeInvoiceStatusRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'status' => [
-                'required',
-                Rule::in([
+            'status' => ['required', Rule::in(self::availableStatus()),],
+        ];
+    }
+
+    private function availableStatus(): array
+    {
+        switch (request('invoice')->status) {
+            case Invoice::STATUS_DRAFT:
+                return [
                     Invoice::STATUS_UNPAID,
+                    Invoice::STATUS_CANCELED,
+                ];
+            case Invoice::STATUS_UNPAID:
+                $statuses = [
                     Invoice::STATUS_CANCELED,
                     Invoice::STATUS_DRAFT,
                     Invoice::STATUS_PAYMENT_PENDING,
                     Invoice::STATUS_COLLECTIONS,
-                    Invoice::STATUS_PAID,
-                ])
-            ],
-        ];
+                ];
+                if (request('invoice')->balance == 0) {
+                    $statuses[] = Invoice::STATUS_PAID;
+                }
+
+                return $statuses;
+            case Invoice::STATUS_CANCELED:
+            case Invoice::STATUS_PAYMENT_PENDING:
+            case Invoice::STATUS_COLLECTIONS:
+                return [
+                    Invoice::STATUS_UNPAID
+                ];
+            case Invoice::STATUS_REFUNDED:
+            case Invoice::STATUS_PAID:
+                return [];
+        }
     }
 }
