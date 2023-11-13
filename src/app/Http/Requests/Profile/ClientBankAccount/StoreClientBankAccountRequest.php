21495ff75556939c2dd1d6f10b7c2bdbeaca69ce
Esmaeel <e.cheshmekhavar@hostiran.com>
2023-11-13 10:53:55 +0330

    IT-6626 Update request validations for ClientBankAccount to require sheba_number , card_number and owner_name to prevent any errors at ClientCashout

--- src/app/Http/Requests/Profile/ClientBankAccount/StoreClientBankAccountRequest.php
+++ src/app/Http/Requests/Profile/ClientBankAccount/StoreClientBankAccountRequest.php
@@ -16,10 +16,10 @@ class StoreClientBankAccountRequest extends FormRequest
         return [
             'client_id' => ['required', 'numeric',],
             'bank_name' => ['nullable', 'string',],
-            'card_number' => ['required_without:sheba_number',],
-            'sheba_number' => ['required_without:card_number', 'regex:/^(?:IR)?(?=.{24}$)[0-9]*$/',],
+            'card_number' => ['required',],
+            'sheba_number' => ['required', 'regex:/^(?:IR)?(?=.{24}$)[0-9]*$/',],
             'account_number' => ['nullable', 'max:255'],
-            'owner_name' => ['nullable', 'max:255',],
+            'owner_name' => ['required', 'max:255',],
         ];
     }
 }
