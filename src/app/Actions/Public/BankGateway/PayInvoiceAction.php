81e0729dd932a5a5a15d59f90a3fb94a462813f1
Esmaeel <e.cheshmekhavar@hostiran.com>
2023-12-05 16:54:49 +0330

    Modify callback url for frontend to provide more detail regarding transaction status

--- src/app/Actions/Public/BankGateway/PayInvoiceAction.php
+++ src/app/Actions/Public/BankGateway/PayInvoiceAction.php
@@ -6,6 +6,7 @@ use App\Actions\Invoice\ProcessInvoiceAction;
 use App\Models\Invoice;
 use App\Models\Transaction;
 use App\Services\BankGateway\MakeBankGatewayProviderByNameService;
+use App\Services\Invoice\CalcInvoicePriceFieldsService;
 use App\Services\Profile\Invoice\StoreTransactionService;
 use Illuminate\Support\Str;
 
@@ -15,29 +16,32 @@ class PayInvoiceAction
         private readonly ProcessInvoiceAction                 $processInvoiceAction,
         private readonly MakeBankGatewayProviderByNameService $makeBankGatewayProviderByNameService,
         private readonly StoreTransactionService              $storeTransactionService,
+        private readonly CalcInvoicePriceFieldsService        $calcInvoicePriceFieldsService,
     )
     {
     }
 
     public function __invoke(Invoice $invoice, string $gatewayName, ?string $source)
     {
-        $redirectTo = $source == 'cloud' ?
+        $rawRedirectUrl = $source == 'cloud' ?
             config('payment.bank_gateway.result_cloud_redirect_url') :
             config('payment.bank_gateway.result_redirect_url');
-        $resultUrl = callback_result_redirect_url($redirectTo, $invoice->id);
 
         // validate Invoice on Whether it should be redirected to bankGateway or not
         // Only Invoices with status of "unpaid","collection","pending" can get redirected to bankGateway
         // otherwise they are either "draft" or "paid" or ... so we will just redirect them to the "result" page
         if (!in_array($invoice->status, [Invoice::STATUS_UNPAID, Invoice::STATUS_COLLECTIONS, Invoice::STATUS_PAYMENT_PENDING])) {
-            return $resultUrl;
+            return callback_result_redirect_url($rawRedirectUrl, $invoice->id, invoiceStatus: $invoice->status);
         }
+
+        $invoice = ($this->calcInvoicePriceFieldsService)($invoice);
+
         // If Invoice is already paid in full but for some reason its status is not set accordingly,  e.g. "paid"
         // then this Invoice should be processed and then redirected to result page
         if ($invoice->balance <= 0) {
-            ($this->processInvoiceAction)($invoice);
+            $invoice = ($this->processInvoiceAction)($invoice);
 
-            return $resultUrl;
+            return callback_result_redirect_url($rawRedirectUrl, $invoice->id, invoiceStatus: $invoice->status);
         }
 
         // Start the BankGateway process:
