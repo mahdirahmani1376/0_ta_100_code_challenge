f2c0bff228894a518af1665a24e30207e469a7e8
Esmaeel <e.cheshmekhavar@hostiran.com>
2023-12-27 14:51:01 +0330

    Load more data inside ShowCreditTransactionResource.php

--- src/app/Http/Resources/Wallet/CreditTransaction/ShowCreditTransactionResource.php
+++ src/app/Http/Resources/Wallet/CreditTransaction/ShowCreditTransactionResource.php
@@ -14,7 +14,14 @@ class ShowCreditTransactionResource extends JsonResource
         /** @var CreditTransaction $this */
         return [
             'id' => $this->id,
+            'created_at' => $this->created_at?->toDateTimeString(),
+            'updated_at' => $this->updated_at?->toDateTimeString(),
+            'profile_id' => $this->profile_id,
+            'wallet_id' => $this->wallet_id,
             'invoice_id' => $this->invoice_id,
+            'admin_id' => $this->admin_id,
+            'amount' => $this->amount,
+            'description' => $this->description,
             'invoice' => $this->when($this->invoice, function () {
                 return ShowInvoiceResource::make($this->invoice);
             })
