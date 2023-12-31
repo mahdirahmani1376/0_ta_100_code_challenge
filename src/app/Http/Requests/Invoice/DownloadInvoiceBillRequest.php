4ab938a0935ad277108b8ec31e16ec8659d9685f
Esmaeel <e.cheshmekhavar@hostiran.com>
2023-12-31 18:51:55 +0330

    Update DownloadInvoiceBillRequest to make "admin_id" field nullable

--- src/app/Http/Requests/Invoice/DownloadInvoiceBillRequest.php
+++ src/app/Http/Requests/Invoice/DownloadInvoiceBillRequest.php
@@ -14,7 +14,7 @@ class DownloadInvoiceBillRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'admin_id' => ['required', 'integer',],
+            'admin_id' => ['nullable', 'integer',],
         ];
     }
 }
