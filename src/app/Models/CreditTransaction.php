2897ee1ae70d0dfb80cec888f978da1be539801b
Esmaeel <e.cheshmekhavar@hostiran.com>
2024-01-23 12:19:39 +0330

    Make CreditTransaction Soft deletable

--- src/app/Models/CreditTransaction.php
+++ src/app/Models/CreditTransaction.php
@@ -6,6 +6,7 @@ use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 /**
  * Class CreditTransaction
@@ -26,7 +27,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
  */
 class CreditTransaction extends Model
 {
-    use HasFactory;
+    use SoftDeletes;
 
     protected $fillable = [
         'profile_id',
--- src/database/migrations/2023_07_09_093843_create_credit_transactions_table.php
+++ src/database/migrations/2023_07_09_093843_create_credit_transactions_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('credit_transactions', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->softDeletes();
             $table->unsignedBigInteger('profile_id');
             $table->unsignedBigInteger('wallet_id');
             $table->unsignedBigInteger('invoice_id')->nullable();
