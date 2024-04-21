1f42983a61ceb40d45c8cf46d54fba0459996a3b
ma.rahmani <ma.rahmani@hostiran.com>
2024-04-21 16:12:30 +0330

    refactor UpdateInvoiceItemsCommand.php and implement FinanceLog

--- src/app/Console/Commands/UpdateInvoiceItemsCommand.php
+++ src/app/Console/Commands/UpdateInvoiceItemsCommand.php
@@ -4,6 +4,7 @@ namespace App\Console\Commands;
 
 use App\Actions\Invoice\Item\UpdateItemAction;
 use App\Integrations\MainApp\MainAppAPIService;
+use App\Models\FinanceLog;
 use App\Models\Invoice;
 use App\Models\Item;
 use Illuminate\Console\Command;
@@ -34,48 +35,54 @@ class UpdateInvoiceItemsCommand extends Command
         $this->info("count of unpaid invoices: {$unpaidInvoices->count()}");
 
         $unpaidInvoices->each(function (Invoice $invoice) {
-            $invoice->items->each(function (Item $item) use ($invoice) {
-                try {
-
-                    if ($item->invoiceable_type == Item::TYPE_DOMAIN) {
-                        $data = MainAppAPIService::recalculateDomainServicePrice($item->invoiceable_id);
-
-                        if (!empty(data_get($data, 'price'))) {
-                            ($this->updateItemAction)($invoice, $item, [
-                                'amount' => $data['price']
-                            ]);
-                        }
-                    }
-
-                    if ($item->invoiceable_type == Item::TYPE_PRODUCT_SERVICE) {
-                        $data = MainAppAPIService::recalculateProductServicePrice($item->invoiceable_id);
-
-                        if (!empty(data_get($data, 'cost'))) {
-                            ($this->updateItemAction)($invoice, $item, [
-                                'amount' => $data['cost']
-                            ]);
-                        }
-                    }
-
-
-                    $this->info("Item: $item->id updated successfully for invoice:$invoice->id changes: " . json_encode($item->getChanges()));
-                    Log::info("Item: $item->id updated successfully for invoice:$invoice->id", [
-                        'changes' => $item->getChanges(),
-                    ]);
-
-                } catch (Throwable $e) {
-                    $this->error("item with id:$item->id with invoice $invoice->id threw exception with message {$e->getMessage()}");
-                    Log::error("item with id:$item->id with invoice $invoice->id threw exception", [
-                        'response' => $data,
-                        'error'    => $e->getMessage(),
-                        'trace'    => $e->getTrace()
-                    ]);
-                }
-            });
+            $invoice->items
+                ->whereIn('invoiceable_type', [Item::TYPE_DOMAIN,Item::TYPE_PRODUCT_SERVICE])
+                    ->each(function (Item $item) use ($invoice) {
+                        $this->updateItem($item, $invoice);
+                    });
         });
 
         $this->info('Completed');
 
+    }
+
+    private function updateItem(Item $item, Invoice $invoice): void
+    {
+        try {
+            $oldState = $invoice->toArray();
+
+            if ($item->invoiceable_type == Item::TYPE_DOMAIN){
+                $item->invoiceable_id = 150792;
+                $response = MainAppAPIService::recalculateDomainServicePrice($item->invoiceable_id);
+                $price = $response;
+            }
+            elseif ($item->invoiceable_type == Item::TYPE_PRODUCT_SERVICE){
+                $item->invoiceable_id = 44;
+
+                $response = MainAppAPIService::recalculateProductServicePrice($item->invoiceable_id);
+                $price = data_get($response, 'cost');
+            }
+
+            if (!empty($price) and !empty($response)){
+                ($this->updateItemAction)($invoice, $item, [
+                    'amount' => $price
+                ]);
+
+                $finance_log = finance_log(FinanceLog::EDIT_INVOICE_ITEM, $item, $item->getChanges(), $oldState, $response);
+
+                $this->info("Item: $item->id updated successfully for invoice:$invoice->id changes: " . json_encode($item->getChanges()));
+                Log::info("Item: $item->id updated successfully for invoice:$invoice->id", [
+                    'changes' => $item->getChanges(),
+                ]);
+            }
+        } catch (Throwable $e) {
+            $this->error("item with id:$item->id with invoice $invoice->id threw exception with message {$e->getMessage()}");
+            Log::error("item with id:$item->id with invoice $invoice->id threw exception", [
+                'response' => $response,
+                'error'    => $e->getMessage(),
+                'trace'    => $e->getTrace()
+            ]);
+        }
     }
 
 }
