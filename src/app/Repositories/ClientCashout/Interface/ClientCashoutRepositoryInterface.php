24a342f0632a586b0d814f4d569ed6a74d07e9fa
ma.rahmani <ma.rahmani@hostiran.com>
2024-05-06 12:21:03 +0330

    fix loop in ImportProductsToRahkaranCommand.php

--- src/app/Repositories/ClientCashout/Interface/ClientCashoutRepositoryInterface.php
+++ src/app/Repositories/ClientCashout/Interface/ClientCashoutRepositoryInterface.php
@@ -2,8 +2,13 @@
 
 namespace App\Repositories\ClientCashout\Interface;
 
+use App\Models\ClientCashout;
 use App\Repositories\Base\Interface\EloquentRepositoryInterface;
+use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @method ClientCashout update(Model $object, array $attributes, array $fillable = [])
+ */
 interface ClientCashoutRepositoryInterface extends EloquentRepositoryInterface
 {
 }
