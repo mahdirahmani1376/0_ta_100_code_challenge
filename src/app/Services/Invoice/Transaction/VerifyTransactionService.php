ddfa88b1f44d37536bd3771a041c10f55cec9aa8
moh3n <m.alizadeh@hostiran.com>
2024-05-08 14:19:47 +0330

    [FIX] IT-9643 : update invoice payment method by last transaction payment method

--- src/app/Services/Invoice/Transaction/VerifyTransactionService.php
+++ src/app/Services/Invoice/Transaction/VerifyTransactionService.php
@@ -2,17 +2,26 @@
 
 namespace App\Services\Invoice\Transaction;
 
+use App\Actions\Invoice\UpdateInvoiceAction;
 use App\Models\Transaction;
 use App\Repositories\Transaction\Interface\TransactionRepositoryInterface;
 
 class VerifyTransactionService
 {
-    public function __construct(private readonly TransactionRepositoryInterface $transactionRepository)
+    public function __construct(
+        private readonly TransactionRepositoryInterface $transactionRepository,
+        private readonly UpdateInvoiceAction            $updateInvoiceAction
+    )
     {
     }
 
     public function __invoke(Transaction $transaction)
     {
+        // update invoice payment method from last success transaction
+        ($this->updateInvoiceAction)($transaction->invoice, [
+            'payment_method' => $transaction->payment_method
+        ]);
+
         return $this->transactionRepository->update(
             $transaction,
             ['status' => Transaction::STATUS_SUCCESS,],
