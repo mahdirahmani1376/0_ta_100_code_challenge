1da8c1de2cfafc1a1edbb92d0f2a86459203aded
moh3n <m.alizadeh@hostiran.com>
2024-05-11 12:26:07 +0330

    [FIX] IT-9643 : Remove some transactions statuses (STATUS_OPG_PAID,STATUS_IPG_PAID)

--- src/app/Models/Transaction.php
+++ src/app/Models/Transaction.php
@@ -44,8 +44,6 @@ class Transaction extends Model
     ];
     const STATUS_PENDING = 'pending'; // old status = 0
     const STATUS_SUCCESS = 'success'; // old status = 1
-    public const STATUS_OPG_PAID = 27;
-    public const STATUS_IPG_PAID = 8;
     const STATUS_FAIL = 'fail'; // old status = 2
     const STATUS_REFUND = 'refund'; // old status 30
 
