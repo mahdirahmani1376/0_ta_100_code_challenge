db396574a76b871616c3e89ec11166e2cf5567fc
ma.rahmani <ma.rahmani@hostiran.com>
2024-06-01 15:44:27 +0330

    fix some bugs

--- src/app/Services/ClientLog/ClientLogService.php
+++ src/app/Services/ClientLog/ClientLogService.php
@@ -3,20 +3,22 @@
 namespace App\Services\ClientLog;
 
 use App\Models\ClientLog;
-use MongoDB\Laravel\Eloquent\Model;
+use Illuminate\Database\Eloquent\Model;
 
 class ClientLogService
 {
     public function updateLog(
         $logId,
         ?Model $model,
+        $before = [],
+        $after = [],
         $action = null
     )
     {
         $logModel = ClientLog::find($logId);
 
         if ($logModel) {
-            $logModel->update($logModel, [
+            $logModel->update([
                 'action'        => $action,
                 'loggable_id'   => $model?->getKey(),
                 'loggable_type' => $model?->getMorphClass()
