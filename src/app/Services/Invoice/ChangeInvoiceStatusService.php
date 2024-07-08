1712266f600cdd49896da208da90d7dbc9c5f2e0
ma.rahmani <ma.rahmani@hostiran.com>
2024-07-08 13:58:04 +0330

    change invoice paid_at to now in ChangeInvoiceStatusService

--- src/app/Services/Invoice/ChangeInvoiceStatusService.php
+++ src/app/Services/Invoice/ChangeInvoiceStatusService.php
@@ -14,7 +14,7 @@ class ChangeInvoiceStatusService
     public function __invoke(Invoice $invoice, string $status): Invoice
     {
         $data = ['status' => $status];
-        if ($invoice->status === Invoice::STATUS_COLLECTIONS and is_null($invoice->paid_at)){
+        if ($invoice->status === Invoice::STATUS_COLLECTIONS && is_null($invoice->paid_at)){
             $data['paid_at'] = now();
         }
         return $this->invoiceRepository->update($invoice, $data, ['status','paid_at']);
