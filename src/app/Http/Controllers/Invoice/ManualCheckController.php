38ecd2a73697856838bba50fc2c74c680c287c26
moh3n <m.alizadeh@hostiran.com>
2024-08-17 14:22:45 +0330

    [FIX] IT-10361 : Fix get config from main app logging

--- src/app/Http/Controllers/Invoice/ManualCheckController.php
+++ src/app/Http/Controllers/Invoice/ManualCheckController.php
@@ -6,6 +6,7 @@ use App\Actions\Invoice\ManualCheckAction;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Invoice\ManualCheckRequest;
 use App\Http\Resources\Invoice\InvoiceResource;
+use App\Integrations\MainApp\MainAppConfig;
 use App\Models\Invoice;
 
 class ManualCheckController extends Controller
@@ -22,6 +23,8 @@ class ManualCheckController extends Controller
      */
     public function __invoke(Invoice $invoice, ManualCheckRequest $request)
     {
+        $test = MainAppConfig::get(key: MainAppConfig::FINANCE_SERVICE_DEFAULT_TAX, refresh: true);
+        dd($test);
         $invoice = ($this->manualCheckAction)($invoice, $request->validated('admin_id'));
 
         return InvoiceResource::make($invoice);
